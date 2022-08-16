0b86e6cd9f67ce096dedb2323465e6cfc43e870e
sudeephb <sudeep.bhandari@canonical.com>
2022-08-16 21:43:49 +0545

    Added jammy support

--- rename.sh
+++ rename.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 charm=$(grep -E "^name:" metadata.yaml | awk '{print $2}')
-echo "renaming ${charm}_*.charm to ${charm}.charm"
+echo "renaming ${charm}_*.charm to ${charm}_series.charm"
 echo -n "pwd: "
 pwd
 ls -al
@@ -9,5 +9,8 @@ if [[ -e "${charm}.charm" ]];
 then
     rm "${charm}.charm"
 fi
-echo "Renaming charm here."
-mv ${charm}_*.charm ${charm}.charm
+# Note(sudeephb): As two different charm files are built, 
+# these are named according to the series they were built on.
+echo "Renaming charms here."
+mv ${charm}_ubuntu-20.04-amd64_ubuntu-18.04-amd64.charm ${charm}_focal.charm
+mv ${charm}_ubuntu-22.04-amd64.charm ${charm}_jammy.charm
