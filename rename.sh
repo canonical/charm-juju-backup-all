939b4d4f67c7c5c221a0f2bd97a5952b097d79c1
james_lin <james.lin@canonical.com>
2024-01-25 16:26:17 +0800

    ci: enable github workflow (#21)
    * ci: enable github workflow
    
    * ci: enable github workflow
    
    * Create different builds for focal and jammy.
    
    Build two different charm files because the one built on jammy doesn't run
    on focal and vice versa.
    
    * Use self-hosted runner to run functional tests
    
    Also specify 'xlarge' runner to avoid running out of storage
    
    ---------
    
    Co-authored-by: sudeephb <sudeep.bhandari@canonical.com>

--- rename.sh
+++ rename.sh
@@ -1,17 +1,22 @@
-#!/bin/bash
-# This is a template `rename.sh` file for ops charms
-# This file is managed by bootstack-charms-spec and should not be modified
-# within individual charm repos. https://launchpad.net/bootstack-charms-spec
-
 charm=$(grep -E "^name:" metadata.yaml | awk '{print $2}')
-echo "renaming ${charm}_*.charm to ${charm}.charm"
+echo "renaming ${charm}_*.charm to ${charm}_series.charm"
 echo -n "pwd: "
 pwd
 ls -al
-echo "Removing previous charm if it exists"
+echo "Removing previous charms if they exist"
 if [[ -e "${charm}.charm" ]];
 then
     rm "${charm}.charm"
 fi
-echo "Renaming charm here."
-mv ${charm}_*.charm ${charm}.charm
+if [[ -e "${charm}_22.04.charm" ]];
+then
+    rm "${charm}_22.04.charm"
+fi
+if [[ -e "${charm}_20.04.charm" ]];
+then
+    rm "${charm}_20.04.charm"
+fi
+
+echo "Renaming charms here."
+mv ${charm}_*22.04*.charm ${charm}_22.04.charm
+mv ${charm}_*20.04*.charm ${charm}_20.04.charm
